2f5fd1737bfffb892b27055629aee3851f700021
Seonghun Park <shp04078@naver.com>
2026-03-26 22:01:54 +0900

    예비군커밋 2

---
+++ 00_am/VUE/test-vue-app/src/components/CheckboxItem3.vue
@@ -0,0 +1,17 @@
+<template>
+  <li><input type="checkbox" :checked="checked" />{{ id }} - {{ name }}</li>
+</template>
+<script>
+export default {
+  name: 'CheckboxItem',
+  props: {
+    id: [Number, String], // Number 또는 String 타입
+    name: String, // String 타입
+    checked: {
+      type: Boolean, // Boolean 타입
+      required: false, // 옵션
+      default: false, // 생략시, 기본값은 false
+    },
+  },
+};
+</script>
---
+++ 00_am/VUE/test-vue-app/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue';
+import App from './App5.vue';
+import mitt from 'mitt';
+
+const emitter = mitt();
+emitter.on('*', (type, e) => console.log(`## 이벤트 로그: ${type} ->`, e));
+
+const app = createApp(App);
+app.config.globalProperties.emitter = emitter;
+app.mount('#app');
