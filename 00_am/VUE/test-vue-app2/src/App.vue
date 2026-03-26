0f9982f04e28ad7780ddcc8f0117169a3b3ca6cf
Seonghun Park <shp04078@naver.com>
2026-03-26 22:25:33 +0900

    예비군 3

---
+++ 00_am/VUE/test-vue-app2/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul>
+      <CheckboxItem
+        v-for="idol in idols"
+        :key="idol.id"
+        :name="idol.name"
+        :checked="idol.checked"
+        :id="idol.id"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import CheckboxItem from './components/CheckboxItem.vue';
+
+export default {
+  name: 'App',
+  // import한 컴포넌트 등록시켜주어야 함. import한 이름과 동일하게 써주세요.
+  components: { CheckboxItem },
+  data() {
+    return {
+      idols: [
+        { id: 1, name: 'BTS', checked: true },
+        { id: 2, name: 'Black Pink', checked: false },
+        { id: 3, name: 'EXO', checked: false },
+        { id: 4, name: 'ITZY', checked: false },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ 00_am/VUE/test-vue-app2/src/components/CheckboxItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <li><input type="checkbox" :checked="checked" />{{ name }}</li>
+</template>
+
+<script>
+export default {
+  name: 'CheckboxItem',
+  //자식 컴포넌트는 부모가 보낸 속성들(props)을 받아야함.
+  //부모가 3개를 보냈으면 자식은 3개를 받음.
+  //부모 :속성명과 동일하게 자식의 props의 ['속성명']으로 써주어야함.
+  //부모로 부터 props로 받은 속성값들은 출력만 가능!!
+  props: ['name', 'checked', 'id'],
+};
+</script>
