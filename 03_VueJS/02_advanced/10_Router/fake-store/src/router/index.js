7bf838b5a25543b4430c0ee3f312335c0d0f8154
shpark47 <shp04078@naver.com>
2026-04-02 17:49:08 +0900

    04.02 심화 기본골대 커밋

--- 03_VueJS/02_advanced/10_Router/fake-store/src/router/index.js
+++ 03_VueJS/02_advanced/10_Router/fake-store/src/router/index.js
@@ -1,8 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import ElectronicsPage from '@/pages/ElectronicsPage.vue';
+import HomePage from '@/pages/HomePage.vue';
+import JeweleryPage from '@/pages/JeweleryPage.vue';
+import MensClothingPage from '@/pages/MensClothingPage.vue';
+import WomensClothingPage from '@/pages/WomensClothingPage.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: '/',
+      component: HomePage,
+      name: 'home',
+    },
+    {
+      path: '/electronics',
+      component: ElectronicsPage,
+      name: 'electronics',
+    },
+    {
+      path: '/jewelery',
+      component: JeweleryPage,
+      name: 'jewelery',
+    },
+    {
+      path: '/mensclothing',
+      component: MensClothingPage,
+      name: "men's clothing",
+    },
+    {
+      path: '/womensclothing',
+      component: WomensClothingPage,
+      name: "women's clothing",
+    },
+  ],
+});
 
-export default router
+export default router;
