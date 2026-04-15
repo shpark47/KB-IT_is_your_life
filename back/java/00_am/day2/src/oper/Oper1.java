4ff4708c388a6e25817cbbd028cc9b370f059972
seonghun <shp04078@naver.com>
2026-04-15 10:51:59 +0900

    Cleanup

---
+++ back/java/00_am/day2/src/oper/Oper1.java
@@ -0,0 +1,14 @@
+package oper;
+
+public class Oper1 {
+//    int x = 10;
+
+    public static void main(String[] args) {
+//        int y = 20;
+        int x = 200;
+        int y = 200;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
