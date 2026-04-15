daa814aa93865fbb7052dac56a1a567b55c59a94
seonghun <shp04078@naver.com>
2026-04-15 17:07:01 +0900

    Fix. 프로그래머스 과제 수정

--- 과제/java_week_2/src/ch04/sec02/IfElseIfElseExample.java
+++ 과제/java_week_2/src/ch04/sec02/IfElseIfElseExample.java
@@ -12,7 +12,7 @@ public class IfElseIfElseExample {
         } else if (score >= 70) {
             System.out.println("점수가 70~79입니다.");
             System.out.println("등급은 C입니다.");
-        } else {
+        } else if (score >= 60){
             System.out.println("점수가 60~69입니다.");
             System.out.println("등급은 D입니다.");
         }
