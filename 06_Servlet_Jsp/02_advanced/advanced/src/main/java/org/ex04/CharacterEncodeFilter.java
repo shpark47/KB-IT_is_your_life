93953c5907a16e10c61fc7259135411a32c50bad
shpark47 <shp04078@naver.com>
2026-06-11 16:31:53 +0900

    Study. 06.11 심화실습 커밋

---
+++ 06_Servlet_Jsp/02_advanced/advanced/src/main/java/org/ex04/CharacterEncodeFilter.java
@@ -0,0 +1,14 @@
+package org.ex04;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CharacterEncodeFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+}
---
+++ 06_Servlet_Jsp/02_advanced/advanced/src/main/java/org/scoula/ex03/BoardServlet.java
@@ -0,0 +1,32 @@
+package org.scoula.ex03;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+@WebServlet("/board")
+public class BoardServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.print("<html><body>");
+        Enumeration<String> enu = req.getParameterNames();
+        while (enu.hasMoreElements()) {
+            String name = enu.nextElement();
+            String value = req.getParameter(name);
+            out.print(name + " : " + value + "<br>");
+        }
+        out.print("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
